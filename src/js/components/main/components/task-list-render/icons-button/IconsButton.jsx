c88c72c58b367c2b18b946d8550b24d8bc39a53c
Kirill Zhiganov <zhigano_k.n@mail.ru>
2023-10-13 08:34:38 +0300

    Начал поправлять

--- src/js/components/main/components/task-list-render/icons-button/IconsButton.jsx
+++ src/js/components/main/components/task-list-render/icons-button/IconsButton.jsx
@@ -11,50 +11,51 @@ import { CompletedTasks } from '../../../../../context/CompletedTasks';
 import { completeType, taskListType } from '../../../../../store/type';
 
 export const IconsButton = ({ itemData, index }) => {
-    const { dispatchTask } = useContext(TaskListContext);
-    const { dispatchCompleted } = useContext(CompletedTasks);
+  const { dispatchTask } = useContext(TaskListContext);
+  const { dispatchCompleted } = useContext(CompletedTasks);
 
-    const completedTaskOnClick = () => {
-        dispatchTask({ type: taskListType.COMPLETED_TASK, payload: itemData.id });
-        dispatchCompleted({ type: completeType.ADD_NEW_TASK, payload: itemData });
-    };
+  const completedTaskOnClick = () => {
+    dispatchTask({ type: taskListType.COMPLETED_TASK, payload: itemData.id });
+    dispatchCompleted({ type: completeType.ADD_NEW_TASK, payload: itemData });
+  };
 
-    const returnToTaskList = () => {
-        dispatchTask({ type: taskListType.ADD_NEW_TASK, payload: itemData });
-        dispatchCompleted({ type: completeType.RETURN_TASK, payload: itemData.id });
-    };
+  const returnToTaskList = () => {
+    dispatchTask({ type: taskListType.ADD_NEW_TASK, payload: itemData });
+    dispatchCompleted({ type: completeType.RETURN_TASK, payload: itemData.id });
+  };
 
-    const moveCompleteTask = (type) => {
-        dispatchCompleted({
-            type: type,
-            payload: index,
-        });
-    };
+  const moveCompleteTask = (type) => {
+    dispatchCompleted({
+      type: type,
+      payload: index,
+    });
+  };
 
-
-    return (
-        <div>
-            {
-                itemData.isDone ? <>
-                    <button className={style.button}
-                            onClick={() => moveCompleteTask(completeType.MOVE_UP_COMPLETED_TASK)}>
-                        <img src={upImage} alt='' />
-                    </button>
-                    <button className={style.button}
-                            onClick={() => moveCompleteTask(completeType.MOVE_DOWN_COMPLETED_TASK)}>
-                        <img
-                            src={downImage} alt='' />
-                    </button>
-                    <button className={style.button} onClick={returnToTaskList}><img
-                        src={returnImage} alt='' />
-                    </button>
-
-                </> : <button className={style.button} onClick={completedTaskOnClick
-                }><img src={doneImage} alt='' /></button>
-
-            }
-
-
-        </div>
-    );
+  return (
+    <div>
+      {!itemData.isDone ? (
+        <>
+          <button
+            className={style.button}
+            onClick={() => moveCompleteTask(completeType.MOVE_UP_COMPLETED_TASK)}
+          >
+            <img src={upImage} alt="" />
+          </button>
+          <button
+            className={style.button}
+            onClick={() => moveCompleteTask(completeType.MOVE_DOWN_COMPLETED_TASK)}
+          >
+            <img src={downImage} alt="" />
+          </button>
+          <button className={style.button} onClick={completedTaskOnClick}>
+            <img src={doneImage} alt="" />
+          </button>
+        </>
+      ) : (
+        <button className={style.button} onClick={returnToTaskList}>
+          <img src={returnImage} alt="" />
+        </button>
+      )}
+    </div>
+  );
 };
